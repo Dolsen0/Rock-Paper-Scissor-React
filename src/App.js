1a10bdab5839bb7473128e2fb4d57bb322b5949c
dolsen0 <derek.olsen@gmail.com>
2022-09-19 22:31:42 -0400

    start of rock paper scissor game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let choices = ["Rock", "Paper", "Scissors"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rock Paper Scissors</h1>
+
+      <h4>Computer Character</h4>
+      <h4>Computer Choice</h4>
+
+      <h4>Display</h4>
+
+      {choices.map((choice) => {
+        return <button>{choice}</button>;
+      })}
     </div>
   );
 }
