d1f65e87c0d67c7d42fe8aa794f425a258944b55
dolsen0 <derek.olsen@gmail.com>
2022-09-19 23:49:05 -0400

    added randomized update for comp score

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const choices = ["Rock", "Paper", "Scissors"];
   const[playerChoice, setPlayerChoice] = useState("Rock, Paper, or Scissors?")
-  let choices = ["Rock", "Paper", "Scissors"];
+  let randChoice = choices[Math.floor(Math.random() * 3)]
+  const[compChoice, setCompChoice] = useState(randChoice)
+
 
   return (
     <div className="App">
+      <>
       <h1>Rock Paper Scissors</h1>
 
       <h4>Computer Character</h4>
-      <h4>{choices[Math.floor(Math.random() * 3)]}</h4>
-
-      <h3>You win</h3>
-
+      <h4>{compChoice}</h4>
+      
       <h4>{playerChoice}</h4>
 
       {choices.map((choice) => {
         return <button onClick={()=>{
           setPlayerChoice(choice)
+          setCompChoice(randChoice)
         }}>{choice}</button>;
       })}
+    </>
     </div>
   );
 }
