494bcaefa4349b09b46c1fdd5a9393c4ee5e636c
dolsen0 <derek.olsen@gmail.com>
2022-09-19 23:22:00 -0400

    rough outline of app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const[playerChoice, setPlayerChoice] = useState("Rock, Paper, or Scissors?")
   let choices = ["Rock", "Paper", "Scissors"];
 
   return (
@@ -8,12 +10,16 @@ function App() {
       <h1>Rock Paper Scissors</h1>
 
       <h4>Computer Character</h4>
-      <h4>Computer Choice</h4>
+      <h4>{choices[Math.floor(Math.random() * 3)]}</h4>
 
-      <h4>Display</h4>
+      <h3>You win</h3>
+
+      <h4>{playerChoice}</h4>
 
       {choices.map((choice) => {
-        return <button>{choice}</button>;
+        return <button onClick={()=>{
+          setPlayerChoice(choice)
+        }}>{choice}</button>;
       })}
     </div>
   );
